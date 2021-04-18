899f1c965983789f5e809f9edb777478409957cf
Pawel Synowiecki <mail@pjsdev.com>
2021-04-18 17:21:32 +0100

    Add web data model. Closes #2

---
+++ src/main/java/com/pjsdev/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,32 @@
+package com.pjsdev.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate; //OffsetDateTime stores UTC time and offset
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+
+    private BeerStyle beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/com/pjsdev/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.pjsdev.msscbeerservice.web.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
