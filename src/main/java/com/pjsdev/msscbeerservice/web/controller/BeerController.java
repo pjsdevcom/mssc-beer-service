e107d40d04acb776c77341e3e836e4f86195d70e
pjsdevcom <mail@pjsdev.com>
2021-05-25 10:00:49 +0100

    Implement Beer Controller

--- src/main/java/com/pjsdev/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/pjsdev/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,9 @@
 package com.pjsdev.msscbeerservice.web.controller;
 
+import com.pjsdev.msscbeerservice.repositories.BeerRepository;
+import com.pjsdev.msscbeerservice.web.mappers.BeerMapper;
 import com.pjsdev.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -8,28 +11,40 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
 
-        //TODO: impl
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(beerMapper.beerToBeerDto(beerRepository.findById(beerId).get()), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<?> saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
 
-        //TODO: impl
+        beerRepository.save(beerMapper.beerDtoToBeer(beerDto));
+
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity<?> updateBeerById(@PathVariable UUID beerId, @Validated @RequestBody BeerDto beerDto) {
 
-        //TODO: impl
+        beerRepository.findById(beerId).ifPresent(beer -> {
+            beer.setBeerName(beerDto.getBeerName());
+            beer.setBeerStyle(beerDto.getBeerStyle().name());
+            beer.setPrice(beerDto.getPrice());
+            beer.setUpc(beerDto.getUpc());
+
+            beerRepository.save(beer);
+        });
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
