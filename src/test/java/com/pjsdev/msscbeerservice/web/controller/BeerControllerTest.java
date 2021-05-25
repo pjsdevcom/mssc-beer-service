c1c631d05064abf2d8946d0aec5667af8174e588
pjsdevcom <mail@pjsdev.com>
2021-05-25 10:06:22 +0100

    Update Beer Controller Test

--- src/test/java/com/pjsdev/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/pjsdev/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,17 +1,23 @@
 package com.pjsdev.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pjsdev.msscbeerservice.domain.Beer;
+import com.pjsdev.msscbeerservice.repositories.BeerRepository;
 import com.pjsdev.msscbeerservice.web.model.BeerDto;
 import com.pjsdev.msscbeerservice.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,8 +30,14 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeerById() throws Exception {
+
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
