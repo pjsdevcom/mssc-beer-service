8d813c33b09ac619215f00bc89a6dea26c23fb5f
pjsdevcom <mail@pjsdev.com>
2021-07-30 10:19:18 +0100

    Fix Hibernate error

--- src/main/java/com/pjsdev/msscbeerservice/domain/Beer.java
+++ src/main/java/com/pjsdev/msscbeerservice/domain/Beer.java
@@ -23,7 +23,7 @@ public class Beer {
     @GeneratedValue(generator = "UUID") //Hibernate will automatically generate the UUID
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Type(type = "org.hibernate.type.UUIDCharType")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
