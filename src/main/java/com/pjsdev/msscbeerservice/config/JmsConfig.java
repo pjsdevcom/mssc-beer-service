d437f78edd31817ac1219fbfa9230d595fab6d82
pjsdevcom <mail@pjsdev.com>
2021-08-15 17:44:30 +0100

    Configure JMS

---
+++ src/main/java/com/pjsdev/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,19 @@
+package com.pjsdev.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    @Bean // Serialize message content to JSON using TextMessage
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
