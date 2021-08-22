21a69d4388278e990fda357b550f580179362f56
pjsdevcom <mail@pjsdev.com>
2021-08-22 15:32:03 +0100

    Fix Jackson JMS configuration

--- src/main/java/com/pjsdev/msscbeerservice/events/BrewBeerEvent.java
+++ src/main/java/com/pjsdev/msscbeerservice/events/BrewBeerEvent.java
@@ -1,7 +1,9 @@
 package com.pjsdev.msscbeerservice.events;
 
 import com.pjsdev.msscbeerservice.web.model.BeerDto;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class BrewBeerEvent extends BeerEvent {
 
     public BrewBeerEvent(BeerDto beerDto) {
--- src/main/java/com/pjsdev/msscbeerservice/events/NewInventoryEvent.java
+++ src/main/java/com/pjsdev/msscbeerservice/events/NewInventoryEvent.java
@@ -1,7 +1,9 @@
 package com.pjsdev.msscbeerservice.events;
 
 import com.pjsdev.msscbeerservice.web.model.BeerDto;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class NewInventoryEvent extends BeerEvent {
 
     public NewInventoryEvent(BeerDto beerDto) {
